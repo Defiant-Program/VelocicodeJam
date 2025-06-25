c6d57aeb1ad036dd0112a2834836297f14a9eb23
defiantprogram <ci-dev@hotmial.com>
2025-06-25 01:11:38 -0500

    ammo spawns close to you, hats spawn wherever. Special ammo and hat spawns that the alamo every 1.5 minutes

--- Cascarone Crash/Assets/AmmoSpawner.cs
+++ Cascarone Crash/Assets/AmmoSpawner.cs
@@ -6,9 +6,19 @@ public class AmmoSpawner : MonoBehaviour
 {
     [SerializeField] Transform topLeft, topRight, bottomLeft, bottomRight;
     [SerializeField] GameObject eggCartonPrefab;
+    [SerializeField] GameObject hatPrefab;
     [SerializeField] Transform eggCartonParent;
+    [SerializeField] Transform hatParent;
 
-    int spawnTimer;
+    [SerializeField] Transform alamo;
+
+    int eggSpawnTimer;
+    int hatSpawnTimer;
+
+    int alamoSpawnTimer;
+
+    GameObject alammo;
+    GameObject hatalamo;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +28,44 @@ public class AmmoSpawner : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        spawnTimer++;
-        if (spawnTimer > 300)
+        eggSpawnTimer++;
+        if (eggSpawnTimer > 300)
             SpawnAmmo();
+        hatSpawnTimer++;
+        if (hatSpawnTimer > 600)
+            SpawnHat();
+        alamoSpawnTimer++;
+        if (alamoSpawnTimer > 900)
+        {
+            if (!alammo)
+                SpawnAlammo();
+            if (!hatalamo)
+                SpawnHatAlamo();
+        }
+    }
+
+    void SpawnAlammo()
+    {
+        alamoSpawnTimer = 0;
+        alammo = Instantiate(eggCartonPrefab, new Vector3(alamo.position.x - 3, 1.1f, alamo.position.z - 7), Quaternion.identity, eggCartonParent);
+    }
+    void SpawnHatAlamo()
+    {
+        alamoSpawnTimer = 0;
+        hatalamo = Instantiate(hatPrefab, new Vector3(alamo.position.x + 3, 1.1f, alamo.position.z - 7), Quaternion.identity, hatParent);
     }
 
     void SpawnAmmo()
     {
-        spawnTimer = 0;
-        Vector3 cartonSpawnPoint = new Vector3(Random.Range(topLeft.position.x, topRight.position.x), 1 , Random.Range(topLeft.position.z, bottomRight.position.z));
+        eggSpawnTimer = 0;
+        Vector3 cartonSpawnPoint = new Vector3(GameController.GC.player.transform.position.x + Random.insideUnitCircle.x * 20, 1.1f, GameController.GC.player.transform.position.z + Random.insideUnitCircle.y * 20);
         Instantiate(eggCartonPrefab, cartonSpawnPoint, Quaternion.identity, eggCartonParent);
+    }
+
+    void SpawnHat()
+    {
+        hatSpawnTimer = 0;
+        Vector3 hatSpawnPoint = new Vector3(Random.Range(Mathf.Max(topLeft.position.x, GameController.GC.player.transform.position.x - 200), Mathf.Min(topRight.position.x, GameController.GC.player.transform.position.x + 200)), 1, Random.Range(Mathf.Max(topLeft.position.z, GameController.GC.player.transform.position.z - 200), Mathf.Min(bottomRight.position.z, GameController.GC.player.transform.position.z + 200)));
+        Instantiate(hatPrefab, hatSpawnPoint, Quaternion.identity, hatParent);
     }
 }
